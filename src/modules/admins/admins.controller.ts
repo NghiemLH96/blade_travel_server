e8454c476bf1bf37f9095bdfcec8d74e6eafc4c9
Nghiem Le <nghiemle@Nghiems-MacBook-Pro.local>
2024-02-24 08:10:55 +0700

    update dashboard usermanager

--- src/modules/admins/admins.controller.ts
+++ src/modules/admins/admins.controller.ts
@@ -7,7 +7,7 @@ import { mailService, resetPWTemplate } from '../mailer/mailer.service';
 
 @Controller('admins')
 export class AdminsController {
-  constructor(private readonly adminsService: AdminsService ,private tokenSrc: tokenService ,private mailerSvc:mailService) { }
+  constructor(private readonly adminsService: AdminsService, private tokenSrc: tokenService, private mailerSvc: mailService) { }
   @Post()
   async adminLogin(@Body() body: loginAdminDto, @Res() res: Response) {
     try {
@@ -20,10 +20,10 @@ export class AdminsController {
             message,
             data
           })
-        }else{
+        } else {
           return res.status(200).json({
             message,
-            token:this.tokenSrc.createToken(data,"1d")
+            token: this.tokenSrc.createToken(data, "1d")
           })
         }
       }
@@ -35,73 +35,200 @@ export class AdminsController {
   }
 
   @Post('check-login')
-  async checkLogin(@Body() body:{token:string}, @Res() res:Response){
+  async checkLogin(@Body() body: { token: string }, @Res() res: Response) {
     const loginDetail = this.tokenSrc.verify(body.token)
-    console.log("loginDetail",loginDetail);
+    console.log("loginDetail", loginDetail);
     try {
-      const result:Boolean = await this.adminsService.checkLogin(loginDetail)
+      const result: Boolean = await this.adminsService.checkLogin(loginDetail)
       if (result) {
         return res.status(200).json({
-          result:true
+          result: true
         })
-      }else{
+      } else {
         throw result
       }
     } catch (error) {
       return res.status(214).json({
-        result:false
+        result: false
       })
     }
   }
 
-  @Get('get-users')
-  async getUsers(@Res() res:Response){
+  @Post('get-users')
+  async getUsers(@Body() body:{currentPage:number,pageSize:number},@Res() res: Response) {
     try {
-      const result = await this.adminsService.getUsers()
+      const result = await this.adminsService.getUsers(body.currentPage,body.pageSize)
       return res.status(200).json({
-        data:result
+        data: result
       })
     } catch (error) {
       return res.status(413).json({
-        data:error
+        data: error
       })
     }
   }
 
   @Patch('user-status')
-  async changeUserStatus(@Body() user:{userId:number,userStatus:boolean},@Res() res:Response){
+  async changeUserStatus(@Body() user: { userId: number, userStatus: boolean }, @Res() res: Response) {
     try {
       const result = await this.adminsService.updateUserStatus(user)
       return res.status(200).json({
-        message:'Cập nhật trạng thái thành công'
+        message: 'Cập nhật trạng thái thành công'
       })
     } catch (error) {
       return res.status(214).json({
-        message:'Cập nhật trạng thái thất bại'
+        message: 'Cập nhật trạng thái thất bại'
       })
     }
   }
-  
+
   @Patch('reset-pass')
-  async resetPW(@Body() body:{userId:string}, @Res() res:Response){
+  async resetPW(@Body() body: { userId: number }, @Res() res: Response) {
     try {
       const result = await this.adminsService.resetPW(body.userId)
       if (result.data) {
-        this.mailerSvc.sendMail(result.data.email,'Khôi phục mật khẩu',resetPWTemplate.resetPWNotify(result.data.email,result.newPasswords))
+        this.mailerSvc.sendMail(result.data.email, 'Khôi phục mật khẩu', resetPWTemplate.resetPWNotify(result.data.email, result.newPasswords))
         return res.status(200).json({
-          message:'Mật khẩu khôi phục thành công',
-          data:result.data
+          message: 'Mật khẩu khôi phục thành công',
+          data: result.data
         })
-      }else{
+      } else {
         return res.status(214).json({
-          message:'Mật khẩu khôi phục thất bại',
+          message: 'Mật khẩu khôi phục thất bại',
         })
       }
     } catch (error) {
       console.log(error);
       return res.status(500).json({
-        message:'Lỗi mạng'
+        message: 'Lỗi mạng'
       })
     }
   }
-}
+
+  @Patch('update-phone')
+  async updatePhoneNo(@Body() body: { userId: number, newPhoneNo: string }, @Res() res: Response) {
+    try {
+      const result = await this.adminsService.updatePhoneNo(body)
+      if (!result.error) {
+        if (result.result) {
+          return res.status(200).json({
+            message: result.message
+          })
+        } else {
+          return res.status(214).json({
+            message: result.message
+          })
+        }
+      } else {
+        res.status(214).json({
+          message: 'Cập nhật thất bại'
+        })
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        message: 'Lỗi gì đó'
+      })
+    }
+  }
+
+  @Post('users-search')
+  async searchByOption(@Body() body: { status: boolean | null, email: string | null, phone: string | null, currentPage: number, pageSize: number }, @Res() res: Response) {
+    const handleCommand = () => {
+      console.log('body',body);
+      
+          //Tìm kiếm tất cả
+          if (body.status == null && body.email == null && body.phone == null) {
+            return 1;
+          } else {
+            //Tìm kiếm bằng 3 điều kiện
+            if (body.status != null && body.email != null && body.phone != null) {
+              return 2;
+            } else {
+              //status 0
+              if (body.status == null) {
+                //email 1 phone 1
+                if (body.email != null && body.phone != null) {
+                  return 7;
+                }else{
+                  //status 0 email 0
+                  if (body.email == null) {
+                    return 3;
+                  }
+                  //status 0 phone 0
+                  if (body.phone == null) {
+                    return 4;
+                  }
+                }
+              } else {
+                //status 1
+                if (body.email == null && body.phone == null) {
+                  return 5;
+                } else {
+                  //status 1 email 1
+                  if (body.phone == null) {
+                    return 6;
+                  }
+                  //status 1 phone 1
+                  if (body.email == null) {
+                    return 8;
+                  }
+                }
+              }
+            }
+          }
+        }
+        let result: any;
+        let commandKey: number = handleCommand();
+        console.log('commandKey',commandKey);
+        
+        try {
+        switch (commandKey) {
+          case 1:
+            result = await this.adminsService.getUsers(body.currentPage,body.pageSize)
+            break;
+          case 2:
+            result = await this.adminsService.searchByStatusPhoneEmail(body.status, body.email, body.phone, body.currentPage, body.pageSize)
+            break;
+          case 3:
+            result = await this.adminsService.searchByPhone(body.phone, body.currentPage, body.pageSize)
+            break;
+          case 4:
+            result = await this.adminsService.searchByEmail(body.email, body.currentPage, body.pageSize)
+            break;
+          case 5:
+            result = await this.adminsService.searchByStatus(body.status, body.currentPage, body.pageSize)
+            break;
+          case 6:
+            result = await this.adminsService.searchByStatusEmail(body.status, body.email, body.currentPage, body.pageSize)
+            break;
+          case 7:
+            result = await this.adminsService.searchByPhoneEmail(body.email, body.phone, body.currentPage, body.pageSize)
+            break;
+          case 8:
+            result = await this.adminsService.searchByStatusPhone(body.status, body.phone, body.currentPage, body.pageSize)
+            break;
+          default:
+            break;
+        }
+        console.log(result);
+        
+        if (!result.error) {
+          return res.status(200).json({
+            message: result.message,
+            data: result.data,
+            total: result.totalItem
+          })
+        } else {
+          throw result.error
+        }
+      } catch (error) {
+        console.log(error);
+        
+        return res.status(500).json({
+          message: 'Lỗi tìm kiếm',
+          error
+        })
+      }
+    }
+  }
--- src/modules/admins/admins.service.ts
+++ src/modules/admins/admins.service.ts
@@ -39,21 +39,21 @@ export class AdminsService {
     }
   }
 
-  async checkLogin(tokenDetail:loginAdminDto){
+  async checkLogin(tokenDetail: loginAdminDto) {
     try {
       const result = this.prisma.admins.findFirst({
-        where:{
-          AND:[{
-            username:tokenDetail.username
+        where: {
+          AND: [{
+            username: tokenDetail.username
           },
           {
-            password:tokenDetail.password
+            password: tokenDetail.password
           }]
         }
       })
       if (result) {
         return true
-      }else{
+      } else {
         throw false
       }
     } catch (error) {
@@ -61,24 +61,33 @@ export class AdminsService {
     }
   }
 
-  async getUsers(){
+  async getUsers(currentPage:number,pageSize:number) {
     try {
-      const result = this.prisma.users.findMany()
-      return result
+      const skipItem = (currentPage-1)*pageSize
+      const takeItem = pageSize
+      const count = await this.prisma.users.count({})
+      const result = await this.prisma.users.findMany({
+        skip:skipItem,
+        take:takeItem
+      })
+      return {
+        data:result,
+        totalItem:count
+      }
     } catch (error) {
       return error
     }
   }
 
-  async updateUserStatus(user:{userId:number,userStatus:boolean}){
+  async updateUserStatus(user: { userId: number, userStatus: boolean }) {
     try {
       const result = await this.prisma.users.update({
-        where:{
-          id:Number(user.userId)
+        where: {
+          id: Number(user.userId)
         },
-        data:{
-          status:!user.userStatus,
-          updateAt:String(Date.now())
+        data: {
+          status: !user.userStatus,
+          updateAt: String(Date.now())
         }
       })
       return true
@@ -88,27 +97,267 @@ export class AdminsService {
     }
   }
 
-  async resetPW(userId:string){
+  async resetPW(userId: number) {
     try {
-      console.log('id',userId);
-      
-      const newPasswords = String(Math.ceil(Math.random()*Date.now()));
+      console.log('id', userId);
+
+      const newPasswords = String(Math.ceil(Math.random() * Date.now()));
       const result = await this.prisma.users.update({
-        where:{
-          id:Number(userId)
+        where: {
+          id: userId
         },
-        data:{
-          password:hashSync(newPasswords,2)
+        data: {
+          password: hashSync(newPasswords, 2),
+          updateAt: String(Date.now())
         }
       })
-      
+
       return {
-        data:result,
+        data: result,
         newPasswords
       }
     } catch (error) {
       console.log(error);
+      return { error }
+    }
+  }
+
+  async updatePhoneNo(user: { userId: number, newPhoneNo: string }) {
+    try {
+      const checkExist = await this.prisma.users.findFirst({
+        where: {
+          phone: user.newPhoneNo
+        }
+      })
+      if (checkExist) {
+        return {
+          result: false,
+          message: 'Số điện thoại đã tồn tại!'
+        }
+      } else {
+        const result = await this.prisma.users.update({
+          where: {
+            id: user.userId
+          },
+          data: {
+            phone: user.newPhoneNo,
+            updateAt: String(Date.now())
+          }
+        })
+        return {
+          result: true,
+          message: 'Cập nhật số điện thoại thành công'
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      return { error }
+    }
+  }
+
+  //Tìm kiếm theo chỉ định
+  async searchByStatus(status:boolean,currentPage:number,pageSize:number){
+    try {
+      const result = await this.prisma.users.findMany({
+        skip:(currentPage-1)*pageSize,
+        take:pageSize,
+        where:{
+          status:status
+        }
+      })
+      const count = await this.prisma.users.count({
+        where:{
+          status:status
+        }
+      })
+      return {
+        message:'Tìm kiếm thành công',
+        data:result,
+        totalItem:count
+      }
+    } catch (error) {
+      console.log("tìm kiếm theo trạng thái lỗi"), error;
+      return {error}
+    }
+  }
+
+  async searchByEmail(email:string,currentPage:number,pageSize:number){
+    try {
+      const result = await this.prisma.users.findMany({
+        where:{
+          email:{contains:email}
+        },
+        skip:(currentPage-1)*pageSize,
+        take:pageSize
+      })
+      const count = await this.prisma.users.count({
+        where:{
+          email:{contains:email}
+        }
+      })
+      return {
+        message:'Tìm kiếm thành công',
+        data:result,
+        totalItem:count
+      }
+    } catch (error) {
+      console.log("tìm kiếm theo hộp thư lỗi"), error;
+      return {error}
+    }
+  }
+
+  async searchByPhone(phone:string,currentPage:number,pageSize:number){
+    try {
+      const result = await this.prisma.users.findMany({
+        skip:(currentPage-1)*pageSize,
+        take:pageSize,
+        where:{
+          phone:{contains:phone}
+        }
+      })
+      const count = await this.prisma.users.count({
+        where:{
+          phone:{contains:phone}
+        }
+      })
+      return {
+        message:'Tìm kiếm thành công',
+        data:result,
+        totalItem:count
+      }
+    } catch (error) {
+      console.log("tìm kiếm theo số điện thoại lỗi"), error;
+      return {error}
+    }
+  }
+
+  async searchByStatusEmail(status:boolean,email:string,currentPage:number,pageSize:number){
+    try {
+      const result = await this.prisma.users.findMany({
+        skip:(currentPage-1)*pageSize,
+        take:pageSize,
+        where:{
+          status:status,
+          email:{contains:email}
+      }
+      })
+      const count = await this.prisma.users.count({
+        where:{
+            status:status,
+            email:{contains:email}
+        }
+      })
+      return {
+        message:'Tìm kiếm thành công',
+        data:result,
+        totalItem:count
+      }
+    } catch (error) {
+      console.log("tìm kiếm theo trạng thái vào hộp thư lỗi"), error;
+      return {error}
+    }
+  }
+
+  async searchByStatusPhone(status:boolean,phone:string,currentPage:number,pageSize:number){
+    try {
+      const result = await this.prisma.users.findMany({
+        skip:(currentPage-1)*pageSize,
+        take:pageSize,
+        where:{
+          status:status,
+          phone:{contains:phone}
+      }
+      })
+      const count = await this.prisma.users.count({
+        where:{
+            status:status,
+            phone:{contains:phone}
+        }
+      })
+      return {
+        message:'Tìm kiếm thành công',
+        data:result,
+        totalItem:count
+      }
+    } catch (error) {
+      console.log("tìm kiếm theo trạng thái và số điện thoại lỗi"), error;
+      return {error}
+    }
+  }
+
+  async searchByPhoneEmail(email:string,phone:string,currentPage:number,pageSize:number){
+    try {
+      const result = await this.prisma.users.findMany({
+        skip:(currentPage-1)*pageSize,
+        take:pageSize,
+        where:{
+            email:{
+              contains:email
+            },
+            phone:{
+              contains:phone
+            }
+        }
+      })
+      const count = await this.prisma.users.count({
+        where:{
+          email:{
+            contains:email
+          },
+          phone:{
+            contains:phone
+          }
+      }
+      })
+      return {
+        message:'Tìm kiếm thành công',
+        data:result,
+        totalItem:count
+      }
+    } catch (error) {
+      console.log("tìm kiếm theo hộp thư và số điện thoại lỗi"), error;
+      return {error}
+    }
+  }
+  
+  async searchByStatusPhoneEmail(status:boolean,email:string,phone:string,currentPage:number,pageSize:number){
+    try {
+      console.log(email);
       
+      const result = await this.prisma.users.findMany({
+        skip:(currentPage-1)*pageSize,
+        take:pageSize,
+        where:{
+          status:status,
+          email:{
+            contains:email
+          },
+          phone:{
+            contains:phone
+          }
+      }
+      })
+
+      const count = await this.prisma.users.count({
+        where:{
+            status:status,
+            email:{
+              contains:email
+            },
+            phone:{
+              contains:phone
+            }
+        }
+      })
+      console.log('result' , result);
+      
+      return {
+        message:'Tìm kiếm thành công',
+        data:result,
+        totalItem:count
+      }
+    } catch (error) {
+      console.log("tìm kiếm theo tất cả lỗi"), error;
       return {error}
     }
   }
